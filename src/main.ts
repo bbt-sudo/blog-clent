f6bf55bd3cb9a035fa6f0687be7f4aa412036b17
pony <903308991@qq.com>
2023-11-04 11:28:15 +0800

    增加备案号

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,5 @@ import App from './App.vue'
 import router from './router/index.ts'
 import pinia from './store/index.ts'
 import naive from 'naive-ui'
-import localStorage from './utils/storage.ts'
 
 createApp(App).use(router).use(pinia).use(naive).mount('#app')
